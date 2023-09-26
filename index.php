3b1ae268331328f6bf58f459e24b71b373916882
MsNoshin <140832698+MsNoshin@users.noreply.github.com>
2023-09-26 11:00:14 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Management System</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
+	
+</head>
+<body>
+	<nav> 
+		<label class="logo"> W- School </label>
+		<ul> 
+			<li> <a href=""> Home </a></li>
+			<li> <a href=""> Contact </a></li>
+			<li> <a href=""> Admission </a></li>
+			<li> <a href="" class="btn btn-success"> Login </a></li>
+		</ul>
+	</nav>
+
+	<div class="section1">
+		<img class="main_img" src="school_management.jpg">
+		<level class="hehe"> We Teach Students With Care</level>
+
+</body>
+</html>>
